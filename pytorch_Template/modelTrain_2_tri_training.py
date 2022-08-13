0a8da6b6b38c6b0eb8fb0070d15d336ff88b2174
501 <2433229014@qq.com>
2022-08-13 22:44:42 +0800

    tri-training

--- pytorch_Template/modelTrain_2_tri_training.py
+++ pytorch_Template/modelTrain_2_tri_training.py
@@ -15,11 +15,13 @@ from shutil import copyfile
 import argparse
 import os
 from utils import seed_everything
+
+
 class MyDataset(Dataset):
-    def __init__(self, trainX,trainY,split_ratio):
+    def __init__(self, trainX, trainY, split_ratio):
         N = trainX.shape[0]
-       
-        TrainNum = int((N*(1-split_ratio)))
+
+        TrainNum = int((N * (1 - split_ratio)))
         self.x = trainX[:TrainNum]
         self.y = trainY[:TrainNum]
 
@@ -28,17 +30,18 @@ class MyDataset(Dataset):
     def __len__(self):
         return self.len
 
-    def __getitem__(self, idx):     
+    def __getitem__(self, idx):
         x = self.x[idx]
         y = self.y[idx]
-        
+
         return (x, y)
 
+
 class MyTestset(Dataset):
-    def __init__(self, trainX,trainY,split_ratio):
+    def __init__(self, trainX, trainY, split_ratio):
         N = trainX.shape[0]
-       
-        TrainNum = int((N*(1-split_ratio)))
+
+        TrainNum = int((N * (1 - split_ratio)))
         self.x = trainX[TrainNum:]
         self.y = trainY[TrainNum:]
 
@@ -47,21 +50,22 @@ class MyTestset(Dataset):
     def __len__(self):
         return self.len
 
-    def __getitem__(self, idx):     
+    def __getitem__(self, idx):
         x = self.x[idx]
         y = self.y[idx]
-        
+
         return (x, y)
+
 
 class SSL():
 
-    def __init__(self, labelset_X , labelset_Y, unlabelset_X, device="cuda:0") :
+    def __init__(self, labelset_X, labelset_Y, unlabelset_X, device="cuda:0"):
         """
         labelset_X: shape (num,*,*,*,,)
         """
         self.label_data = torch.tensor(labelset_X, dtype=torch.float32)
         self.label = torch.tensor(labelset_Y, dtype=torch.float32)
-        self.unlabel_data= torch.tensor(unlabelset_X, dtype=torch.float32)
+        self.unlabel_data = torch.tensor(unlabelset_X, dtype=torch.float32)
         self.F1 = Model_2()
         self.F2 = Model_2()
         self.F3 = Model_2()
@@ -77,37 +81,39 @@ class SSL():
         # self.unlabelset_left = None
         self.device = device
         # self.model_last = None
+
     def main(self):
-        self.pre_train(pre_epoch=500)
-        for i in range(20):
+        self.pre_train(pre_epoch=200)
+        train_epoch = 100
+        for i in range(train_epoch):
             logging.info(f"第{i}次循环")
-            self.formal_train(epoch=i,TOTAL_EPOCHS=1000)
+            self.formal_train(epoch=i, num=train_epoch)
         logging.info("训练model_last   ###################################")
-        self.model_last = self.train(self.hybrid_data1, self.hybrid_label1, self.F1, TOTAL_EPOCHS=1000)
+        self.model_last = self.train(self.hybrid_data1, self.hybrid_label1, self.F1, TOTAL_EPOCHS=100)
 
-
-    def train(self, train_X, train_Y, model, TOTAL_EPOCHS=20, BATCH_SIZE=100, LEARNING_RATE=0.001, change_learning_rate_epochs=100):
+    def train(self, train_X, train_Y, model, TOTAL_EPOCHS=50, BATCH_SIZE=100, LEARNING_RATE=0.001,
+              change_learning_rate_epochs=100):
         model = model.to(self.device)
-        train_dataset = MyDataset(train_X,train_Y,split_ratio=0)
+        train_dataset = MyDataset(train_X, train_Y, split_ratio=0)
         train_loader = DataLoader(dataset=train_dataset,
-                                                batch_size=BATCH_SIZE,
-                                                shuffle=True)  # shuffle 标识要打乱顺序
+                                  batch_size=BATCH_SIZE,
+                                  shuffle=True)  # shuffle 标识要打乱顺序
         criterion = nn.MSELoss().to(self.device)
         optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=2e-4)
         for epoch in range(TOTAL_EPOCHS):
-            model.train()       
-            optimizer.param_groups[0]['lr'] = LEARNING_RATE /np.sqrt(np.sqrt(epoch+1))
+            model.train()
+            optimizer.param_groups[0]['lr'] = LEARNING_RATE / np.sqrt(np.sqrt(epoch + 1))
             # Learning rate decay
             if (epoch + 1) % change_learning_rate_epochs == 0:
-                optimizer.param_groups[0]['lr'] /= 2 
+                optimizer.param_groups[0]['lr'] /= 2
                 logging.info('lr:%.4e' % optimizer.param_groups[0]['lr'])
-            
-            #Training in this epoch  
+
+            # Training in this epoch
             loss_avg = 0
             for i, (x, y) in enumerate(train_loader):
                 x = x.float().to(self.device)
                 y = y.float().to(self.device)
-                
+
                 # 清零
                 optimizer.zero_grad()
                 output = model(x)
@@ -115,9 +121,9 @@ class SSL():
                 loss = criterion(output, y)
                 loss.backward()
                 optimizer.step()
-                
-                loss_avg += loss.item() 
-                
+
+                loss_avg += loss.item()
+
             loss_avg /= len(train_loader)
             logging.info('Epoch : %d/%d, Loss: %.4f' % (epoch + 1, TOTAL_EPOCHS, loss_avg))
         return model
@@ -164,42 +170,41 @@ class SSL():
         logging.info("训练F1、F2、F3   ##################################")
         hybrid_data1 = torch.concat((self.label_data, self.unlabel_data1), dim=0)
         hybrid_label1 = torch.concat((self.label, pseudo_label1), dim=0)
-        self.F1 = self.train(hybrid_data1, hybrid_label1, self.F1, TOTAL_EPOCHS=pre_epoch)
+        self.F1 = self.train(hybrid_data1, hybrid_label1, self.F1)
 
         hybrid_data2 = torch.concat((self.label_data, self.unlabel_data2), dim=0)
         hybrid_label2 = torch.concat((self.label, pseudo_label2), dim=0)
-        self.F2 = self.train(hybrid_data2, hybrid_label2, self.F2, TOTAL_EPOCHS=pre_epoch)
+        self.F2 = self.train(hybrid_data2, hybrid_label2, self.F2)
 
         hybrid_data3 = torch.concat((self.label_data, self.unlabel_data3), dim=0)
         hybrid_label3 = torch.concat((self.label, pseudo_label3), dim=0)
-        self.F3 = self.train(hybrid_data3, hybrid_label3, self.F3, TOTAL_EPOCHS=pre_epoch)
+        self.F3 = self.train(hybrid_data3, hybrid_label3, self.F3)
 
-
-    def formal_train(self, epoch,TOTAL_EPOCHS=20):
+    def formal_train(self, epoch, num):
         """分割未标签数据"""
-        self.split_unlabelset(epoch+1)
+        # self.split_unlabelset(epoch+1)
 
         logging.info("正式训练F1、F2、F3   ##################################")
-        pseudo_label_data1, pseudo_label1 = self.generate_pseudo_label(self.F2, self.F3, self.unlabel_data1)
+        pseudo_label_data1, pseudo_label1 = self.generate_pseudo_label(self.F2, self.F3, self.unlabel_data,
+                                                                       percentage=(epoch + 1) / num)
         logging.info(f"pseudo_label_data1.shape[0]:{pseudo_label_data1.shape[0]}")
         self.hybrid_data1 = torch.concat((self.label_data, pseudo_label_data1), dim=0)
         self.hybrid_label1 = torch.concat((self.label, pseudo_label1), dim=0)
-        logging.info("正式训练F1 ##################################")
-        self.F1 = self.train(self.hybrid_data1, self.hybrid_label1, self.F1, TOTAL_EPOCHS=TOTAL_EPOCHS)
+        self.F1 = self.train(self.hybrid_data1, self.hybrid_label1, self.F1)
 
-        pseudo_label_data2, pseudo_label2 = self.generate_pseudo_label(self.F1, self.F3, self.unlabel_data2)
+        pseudo_label_data2, pseudo_label2 = self.generate_pseudo_label(self.F1, self.F3, self.unlabel_data,
+                                                                       percentage=(epoch + 1) / num)
         logging.info(f"pseudo_label_data2.shape[0]:{pseudo_label_data2.shape[0]}")
         self.hybrid_data2 = torch.concat((self.label_data, pseudo_label_data2), dim=0)
         self.hybrid_label2 = torch.concat((self.label, pseudo_label2), dim=0)
-        logging.info("正式训练F2   ##################################")
-        self.F2 = self.train(self.hybrid_data2, self.hybrid_label2, self.F2, TOTAL_EPOCHS=TOTAL_EPOCHS)
+        self.F2 = self.train(self.hybrid_data2, self.hybrid_label2, self.F2)
 
-        pseudo_label_data3, pseudo_label3 = self.generate_pseudo_label(self.F1, self.F2, self.unlabel_data3)
+        pseudo_label_data3, pseudo_label3 = self.generate_pseudo_label(self.F1, self.F2, self.unlabel_data,
+                                                                       percentage=(epoch + 1) / num)
         logging.info(f"pseudo_label_data3.shape[0]:{pseudo_label_data3.shape[0]}")
         self.hybrid_data3 = torch.concat((self.label_data, pseudo_label_data3), dim=0)
         self.hybrid_label3 = torch.concat((self.label, pseudo_label3), dim=0)
-        logging.info("正式训练F3   ##################################")
-        self.F3 = self.train(self.hybrid_data3, self.hybrid_label3, self.F3, TOTAL_EPOCHS=TOTAL_EPOCHS)
+        self.F3 = self.train(self.hybrid_data3, self.hybrid_label3, self.F3)
 
     def predict_label(self, model, X, BATCH_SIZE=100):
         model.eval()
@@ -217,7 +222,7 @@ class SSL():
                 Y_pselabeled = torch.concat((Y_pselabeled, Y_pselabeled_batch), dim=0)
         return Y_pselabeled
 
-    def generate_pseudo_label(self, model1, model2, unlabel_data):
+    def generate_pseudo_label(self, model1, model2, unlabel_data, percentage):
         output1 = self.predict_label(model1, unlabel_data)
         output2 = self.predict_label(model2, unlabel_data)
 
@@ -228,57 +233,45 @@ class SSL():
         # Unlabeled_X3 = self.unlabelset_X3[diff_flag.logical_not()]
         # Labeled_Y3 = ((Y3_model1 + Y3_model2)/2.0)[diff_flag]
         """方式2:两个模型预测最接近的前一半的样本打上标签"""
-        pseudo_label_data = self.unlabel_data[diff.sort()[1][:len(diff) // 2]]
-        pseudo_label = ((output1 + output2) / 2.0)[diff.sort()[1][:len(diff) // 2]]
-        logging.info(f"diff最小：{diff.sort()[0][0].item():.3f},diff中值：{diff.sort()[0][len(diff) // 2].item():.3f}")
+        pseudo_label_data = self.unlabel_data[diff.sort()[1][:int(len(diff) * percentage)]]
+        pseudo_label = ((output1 + output2) / 2.0)[diff.sort()[1][:int(len(diff) * percentage)]]
         return pseudo_label_data, pseudo_label
-
-
-
 
     # def get_new_set(self,thres=1):
     #     # 训练F1
     #     logging.info("训练F1   ##################################")
 
-        # logging.info("训练model0   ##################################")
-        # self.model0 = self.train(self.labelset_X, self.labelset_Y,TOTAL_EPOCHS=1)
-        # self.split_unlabelset()
-        # Y1 = self.label(self.unlabelset_X1, self.model0)
-        # Y2 = self.label(self.unlabelset_X2, self.model0)
-        # X1_concat = Model_2()
-        # Y1_concat = torch.concat((self.labelset_Y, Y1), dim = 0)
-        # X2_concat = torch.concat((self.labelset_X, self.unlabelset_X2), dim=0)
-        # Y2_concat = torch.concat((self.labelset_Y, Y2), dim=0)
-        # logging.info("训练model1   ##################################")
-        # self.model1 = self.train(X1_concat,Y1_concat,TOTAL_EPOCHS=1)
-        # logging.info("训练model2   ###################################")
-        # self.model2 = self.train(X2_concat,Y2_concat,TOTAL_EPOCHS=1)
-        # Y3_model1 = self.label(self.unlabelset_X3, self.model1)
-        # Y3_model2 = self.label(self.unlabelset_X3, self.model2)
-        # diff = (Y3_model1 - Y3_model2).square().sum(dim=1).sqrt()
-        # """方式1:两个模型预测足够接近的样本才打上标签"""
-        # # diff_flag = diff < thres
-        # # Labeled_X3 = self.unlabelset_X3[diff_flag]
-        # # Unlabeled_X3 = self.unlabelset_X3[diff_flag.logical_not()]
-        # # Labeled_Y3 = ((Y3_model1 + Y3_model2)/2.0)[diff_flag]
-        # """方式2:两个模型预测最接近的前一半的样本打上标签"""
-        # Labeled_X3 = self.unlabelset_X3[diff.sort()[1][:len(diff)//2]]
-        # Unlabeled_X3 = self.unlabelset_X3[diff.sort()[1][len(diff)//2:]]
-        # Labeled_Y3 = ((Y3_model1 + Y3_model2)/2.0)[diff.sort()[1][:len(diff)//2]]
-        #
-        # self.labelset_X = torch.concat((self.labelset_X,Labeled_X3), dim=0)
-        # self.labelset_Y = torch.concat((self.labelset_Y,Labeled_Y3), dim=0)
-        # self.unlabelset_X = torch.concat((self.unlabelset_X1, self.unlabelset_X2, Unlabeled_X3), dim=0)
-        # if len(self.unlabelset_left) > 0:
-        #     self.unlabelset_X = torch.concat((self.unlabelset_X, self.unlabelset_left), dim=0)
-
-
-
-
-
-
-
-
+    # logging.info("训练model0   ##################################")
+    # self.model0 = self.train(self.labelset_X, self.labelset_Y,TOTAL_EPOCHS=1)
+    # self.split_unlabelset()
+    # Y1 = self.label(self.unlabelset_X1, self.model0)
+    # Y2 = self.label(self.unlabelset_X2, self.model0)
+    # X1_concat = Model_2()
+    # Y1_concat = torch.concat((self.labelset_Y, Y1), dim = 0)
+    # X2_concat = torch.concat((self.labelset_X, self.unlabelset_X2), dim=0)
+    # Y2_concat = torch.concat((self.labelset_Y, Y2), dim=0)
+    # logging.info("训练model1   ##################################")
+    # self.model1 = self.train(X1_concat,Y1_concat,TOTAL_EPOCHS=1)
+    # logging.info("训练model2   ###################################")
+    # self.model2 = self.train(X2_concat,Y2_concat,TOTAL_EPOCHS=1)
+    # Y3_model1 = self.label(self.unlabelset_X3, self.model1)
+    # Y3_model2 = self.label(self.unlabelset_X3, self.model2)
+    # diff = (Y3_model1 - Y3_model2).square().sum(dim=1).sqrt()
+    # """方式1:两个模型预测足够接近的样本才打上标签"""
+    # # diff_flag = diff < thres
+    # # Labeled_X3 = self.unlabelset_X3[diff_flag]
+    # # Unlabeled_X3 = self.unlabelset_X3[diff_flag.logical_not()]
+    # # Labeled_Y3 = ((Y3_model1 + Y3_model2)/2.0)[diff_flag]
+    # """方式2:两个模型预测最接近的前一半的样本打上标签"""
+    # Labeled_X3 = self.unlabelset_X3[diff.sort()[1][:len(diff)//2]]
+    # Unlabeled_X3 = self.unlabelset_X3[diff.sort()[1][len(diff)//2:]]
+    # Labeled_Y3 = ((Y3_model1 + Y3_model2)/2.0)[diff.sort()[1][:len(diff)//2]]
+    #
+    # self.labelset_X = torch.concat((self.labelset_X,Labeled_X3), dim=0)
+    # self.labelset_Y = torch.concat((self.labelset_Y,Labeled_Y3), dim=0)
+    # self.unlabelset_X = torch.concat((self.unlabelset_X1, self.unlabelset_X2, Unlabeled_X3), dim=0)
+    # if len(self.unlabelset_left) > 0:
+    #     self.unlabelset_X = torch.concat((self.unlabelset_X, self.unlabelset_left), dim=0)
 
 
 if __name__ == '__main__':
@@ -287,34 +280,33 @@ if __name__ == '__main__':
     parser.add_argument('--cuda', default=0)
     args = parser.parse_args()
     """注意评测设备只有一块gpu"""
-    DEVICE=torch.device(f"cuda:{args.cuda}")
+    DEVICE = torch.device(f"cuda:{args.cuda}")
 
-    id_path = os.path.join('submit',str(args.submit_id))
+    id_path = os.path.join('submit', str(args.submit_id))
     if not os.path.exists(id_path):
         os.mkdir(id_path)
     submit_path = os.path.join(id_path, 'submit_pt')
     if not os.path.exists(submit_path):
         os.mkdir(submit_path)
-    logging.basicConfig(filename=os.path.join(id_path,"model2_log.txt"), filemode='w', level=logging.DEBUG)
-    model_save = os.path.join(submit_path,'modelSubmit_2.pth')
-    # copyfile('modelDesign_2.py', os.path.join(submit_path, 'modelDesign_2.py'))
-    copyfile(__file__, os.path.join(id_path, __file__.split('/')[-1]))
+    logging.basicConfig(filename=os.path.join(id_path, "model2_log.txt"), filemode='w', level=logging.DEBUG)
+    model_save = os.path.join(submit_path, 'modelSubmit_2.pth')
+    copyfile('modelDesign_2.py', os.path.join(submit_path, 'modelDesign_2.py'))
+    # copyfile(__file__, os.path.join(id_path, __file__.split('/')[-1]))
 
     seed_value = 1
     seed_everything(seed_value=seed_value)
     logging.info(f'seed_value:{seed_value}')
 
     file_name1 = 'data/Case_3_Training.npy'
-    logging.info('The current dataset is : %s'%(file_name1))
+    logging.info('The current dataset is : %s' % (file_name1))
     CIR = np.load(file_name1)
-    trainX = CIR.transpose((2,1,3,0))  #[none, 256, 72, 2]
-    trainX_labeled = trainX[:1000,:,:,:]
-    trainX_unlabeled = trainX[1000:,:,:,:]
+    trainX = CIR.transpose((2, 1, 3, 0))  # [none, 256, 72, 2]
+    trainX_labeled = trainX[:1000, :, :, :]
+    trainX_unlabeled = trainX[1000:, :, :, :]
     file_name2 = 'data/Case_3_Training_Label.npy'
-    logging.info('The current dataset is : %s'%(file_name2))
+    logging.info('The current dataset is : %s' % (file_name2))
     POS = np.load(file_name2)
-    trainY_labeled = POS.transpose((1,0)) #[none, 2]
-
+    trainY_labeled = POS.transpose((1, 0))  # [none, 2]
 
     """打乱数据顺序"""
     index_L = np.arange(len(trainX_labeled))
@@ -324,7 +316,7 @@ if __name__ == '__main__':
     index_U = np.arange(len(trainX_unlabeled))
     np.random.shuffle(index_U)
     trainX_unlabeled = trainX_unlabeled[index_U]
-    
+
     ssl = SSL(labelset_X=trainX_labeled, labelset_Y=trainY_labeled, unlabelset_X=trainX_unlabeled, device=DEVICE)
     ssl.main()
     ssl.model_last.to("cuda:0")
